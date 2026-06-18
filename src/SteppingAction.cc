e898c0108309b216fc514adabc114eb93710b552
vka228 <dvikultsev@gmail.com>
2026-06-18 12:08:57 +0300

    dummy energy calculation

--- src/SteppingAction.cc
+++ src/SteppingAction.cc
@@ -1,28 +1,3 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
 /// \file SteppingAction.cc
 /// \brief Implementation of the B1::SteppingAction class
 
@@ -47,24 +22,27 @@ SteppingAction::SteppingAction(EventAction* eventAction) : fEventAction(eventAct
 
 void SteppingAction::UserSteppingAction(const G4Step* step)
 {
-  if (!fScoringVolume) {
-    const auto detConstruction = static_cast<const DetectorConstruction*>(
-      G4RunManager::GetRunManager()->GetUserDetectorConstruction());
-    fScoringVolume = detConstruction->GetScoringVolume();
-  }
-
-  // get volume of the current step
-  G4LogicalVolume* volume =
-    step->GetPreStepPoint()->GetTouchableHandle()->GetVolume()->GetLogicalVolume();
-
-  // check if we are in scoring volume
-  if (volume != fScoringVolume) return;
-
-  // collect energy deposited in this step
-  G4double edepStep = step->GetTotalEnergyDeposit();
-  fEventAction->AddEdep(edepStep);
+    G4LogicalVolume* volume = step->GetPreStepPoint()
+        ->GetTouchableHandle()
+        ->GetVolume()
+        ->GetLogicalVolume();
+    
+    G4String volName = volume->GetName();
+    G4double edepStep = step->GetTotalEnergyDeposit();
+    
+  // accumulating energy in scin
+    if (edepStep > 0) {
+        G4cout << "Step in " << volName 
+               << ": Edep = " << edepStep << " keV" << G4endl;
+    }
+    
+    
+    if (volName.contains("Scint")) {
+        if (edepStep > 0) {
+            fEventAction->AddEdep(edepStep);
+        }
+    }
 }
-
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 }  // namespace B1
