15dffca4cf68c2a801e37f4494fa837b6ab93306
vka228 <dvikultsev@gmail.com>
2026-06-18 11:16:17 +0300

    minimum geometry

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -1,169 +1,102 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-/// \file DetectorConstruction.cc
-/// \brief Implementation of the B1::DetectorConstruction class
-
 #include "DetectorConstruction.hh"
-
 #include "G4Box.hh"
-#include "G4Cons.hh"
 #include "G4LogicalVolume.hh"
-#include "G4NistManager.hh"
 #include "G4PVPlacement.hh"
+#include "G4NistManager.hh"
 #include "G4SystemOfUnits.hh"
-#include "G4Trd.hh"
+#include "G4VisAttributes.hh"
+#include "G4Colour.hh"
+#include "G4String.hh"  // Добавляем для G4String
 
 namespace B1
 {
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 G4VPhysicalVolume* DetectorConstruction::Construct()
 {
-  // Get nist material manager
-  G4NistManager* nist = G4NistManager::Instance();
-  G4Material* air = nist->FindOrBuildMaterial("G4_AIR");
+    // standard import + calorimeter materials
+    G4NistManager* nist = G4NistManager::Instance();
+    G4Material* air = nist->FindOrBuildMaterial("G4_AIR");
+    G4Material* lead = nist->FindOrBuildMaterial("G4_Pb");
+    G4Material* scint = nist->FindOrBuildMaterial("G4_PLASTIC_SC_VINYLTOLUENE");
+    
+    // world definition
+    G4double worldSize = 2.0 * m;
+    G4Box* solidWorld = new G4Box("World", worldSize/2, worldSize/2, worldSize/2);
+    G4LogicalVolume* logicWorld = new G4LogicalVolume(solidWorld, air, "World");
+    G4VPhysicalVolume* physWorld = new G4PVPlacement(0, G4ThreeVector(), 
+                                                     logicWorld, "World", 
+                                                     0, false, 0, true);
+    
+    // calorimeter params
+    G4int nLayers = 30;
+    G4double pbThick = 2.0 * mm;
+    G4double scintThick = 4.0 * mm;
+    G4double calorSize = 30.0 * cm;
+    
+    G4double layerThick = pbThick + scintThick;
+    G4double totalThick = nLayers * layerThick;
+    
+    // air contaier
+    G4Box* solidContainer = new G4Box("Container", 
+                                      calorSize/2, calorSize/2, totalThick/2);
+    G4LogicalVolume* logicContainer = new G4LogicalVolume(solidContainer, air, "Container");
+    new G4PVPlacement(0, G4ThreeVector(0, 0, 0), 
+                     logicContainer, "Container", 
+                     logicWorld, false, 0, true);
+    
+    // calorimeter itself
+    for (G4int i = 0; i < nLayers; i++) {
+        G4double zPos = -totalThick/2 + (i + 0.5) * layerThick;
+        
+        // Pb
+        G4String pbName = "Pb_" + std::to_string(i);
+        G4String pbLVName = "Pb_LV_" + std::to_string(i);
+        
+        G4Box* solidPb = new G4Box(pbName, 
+                                   calorSize/2, calorSize/2, pbThick/2);
+        G4LogicalVolume* logicPb = new G4LogicalVolume(solidPb, lead, pbLVName);
+        new G4PVPlacement(0, G4ThreeVector(0, 0, zPos - scintThick/2), 
+                         logicPb, pbName, 
+                         logicContainer, false, i, true);
 
-  // Envelope parameters
-  //
-  G4double env_sizeXY = 20 * cm, env_sizeZ = 30 * cm;
-  G4Material* env_mat = nist->FindOrBuildMaterial("G4_WATER");
-
-  // Option to switch on/off checking of volumes overlaps
-  //
-  G4bool checkOverlaps = true;
-
-  //
-  // World
-  //
-  G4double world_sizeXY = 1.2 * env_sizeXY;
-  G4double world_sizeZ = 1.2 * env_sizeZ;
-  G4Material* world_mat = nist->FindOrBuildMaterial("G4_AIR");
-
-  auto solidWorld =
-    new G4Box("World",  // its name
-              0.5 * world_sizeXY, 0.5 * world_sizeXY, 0.5 * world_sizeZ);  // its size
-
-  auto logicWorld = new G4LogicalVolume(solidWorld,  // its solid
-                                        world_mat,  // its material
-                                        "World");  // its name
-
-  auto physWorld = new G4PVPlacement(nullptr,  // no rotation
-                                     G4ThreeVector(),  // at (0,0,0)
-                                     logicWorld,  // its logical volume
-                                     "World",  // its name
-                                     nullptr,  // its mother  volume
-                                     false,  // no boolean operation
-                                     0,  // copy number
-                                     checkOverlaps);  // overlaps checking
-
-  //
-  // Envelope
-  //
-  auto solidEnv = new G4Box("Envelope",  // its name
-                            0.5 * env_sizeXY, 0.5 * env_sizeXY, 0.5 * env_sizeZ);  // its size
-
-  auto logicEnv = new G4LogicalVolume(solidEnv,  // its solid
-                                      env_mat,  // its material
-                                      "Envelope");  // its name
-
-  new G4PVPlacement(nullptr,  // no rotation
-                    G4ThreeVector(),  // at (0,0,0)
-                    logicEnv,  // its logical volume
-                    "Envelope",  // its name
-                    logicWorld,  // its mother  volume
-                    false,  // no boolean operation
-                    0,  // copy number
-                    checkOverlaps);  // overlaps checking
-
-  //
-  // Shape 1
-  //
-  G4Material* shape1_mat = nist->FindOrBuildMaterial("G4_A-150_TISSUE");
-  G4ThreeVector pos1 = G4ThreeVector(0, 2 * cm, -7 * cm);
-
-  // Conical section shape
-  G4double shape1_rmina = 0. * cm, shape1_rmaxa = 2. * cm;
-  G4double shape1_rminb = 0. * cm, shape1_rmaxb = 4. * cm;
-  G4double shape1_hz = 3. * cm;
-  G4double shape1_phimin = 0. * deg, shape1_phimax = 360. * deg;
-  auto solidShape1 = new G4Cons("Shape1", shape1_rmina, shape1_rmaxa, shape1_rminb, shape1_rmaxb,
-                                shape1_hz, shape1_phimin, shape1_phimax);
-
-  auto logicShape1 = new G4LogicalVolume(solidShape1,  // its solid
-                                         shape1_mat,  // its material
-                                         "Shape1");  // its name
-
-  new G4PVPlacement(nullptr,  // no rotation
-                    pos1,  // at position
-                    logicShape1,  // its logical volume
-                    "Shape1",  // its name
-                    logicEnv,  // its mother  volume
-                    false,  // no boolean operation
-                    0,  // copy number
-                    checkOverlaps);  // overlaps checking
-
-  //
-  // Shape 2
-  //
-  G4Material* shape2_mat = nist->FindOrBuildMaterial("G4_BONE_COMPACT_ICRU");
-  G4ThreeVector pos2 = G4ThreeVector(0, -1 * cm, 7 * cm);
-
-  // Trapezoid shape
-  G4double shape2_dxa = 12 * cm, shape2_dxb = 12 * cm;
-  G4double shape2_dya = 10 * cm, shape2_dyb = 16 * cm;
-  G4double shape2_dz = 6 * cm;
-  auto solidShape2 =
-    new G4Trd("Shape2",  // its name
-              0.5 * shape2_dxa, 0.5 * shape2_dxb, 0.5 * shape2_dya, 0.5 * shape2_dyb,
-              0.5 * shape2_dz);  // its size
-
-  auto logicShape2 = new G4LogicalVolume(solidShape2,  // its solid
-                                         shape2_mat,  // its material
-                                         "Shape2");  // its name
-
-  new G4PVPlacement(nullptr,  // no rotation
-                    pos2,  // at position
-                    logicShape2,  // its logical volume
-                    "Shape2",  // its name
-                    logicEnv,  // its mother  volume
-                    false,  // no boolean operation
-                    0,  // copy number
-                    checkOverlaps);  // overlaps checking
-
-  // Set Shape2 as scoring volume
-  //
-  fScoringVolume = logicShape2;
-
-  //
-  // always return the physical World
-  //
-  return physWorld;
+        G4VisAttributes* pbVis = new G4VisAttributes(G4Colour(0.3 , 0.3, 0.3));
+        logicPb->SetVisAttributes(pbVis);
+        
+        // scinc 
+        G4String scintName = "Scint_" + std::to_string(i);
+        G4String scintLVName = "Scint_LV_" + std::to_string(i);
+        
+        G4Box* solidScint = new G4Box(scintName, 
+                                      calorSize/2, calorSize/2, scintThick/2);
+        G4LogicalVolume* logicScint = new G4LogicalVolume(solidScint, scint, scintLVName);
+        new G4PVPlacement(0, G4ThreeVector(0, 0, zPos + pbThick/2), 
+                         logicScint, scintName, 
+                         logicContainer, false, i, true);
+        
+        G4VisAttributes* scintVis = new G4VisAttributes(G4Colour(0.2, 0.3 + 0.6*(i%2), 0.8));
+        logicScint->SetVisAttributes(scintVis);
+        
+        if (i == 0) {
+            fScoringVolume = logicScint;
+        }
+    }
+    
+    // visualization
+    G4VisAttributes* worldVis = new G4VisAttributes(G4Colour(0.5, 0.5, 0.5, 0.1));
+    logicWorld->SetVisAttributes(worldVis);
+    logicContainer->SetVisAttributes(G4VisAttributes::GetInvisible());
+    
+    // some logs
+    G4cout << "====================================" << G4endl;
+    G4cout << "Calorimeter constructed:" << G4endl;
+    G4cout << "  Layers: " << nLayers << G4endl;
+    G4cout << "  Pb thickness: " << pbThick/mm << " mm" << G4endl;
+    G4cout << "  Scint thickness: " << scintThick/mm << " mm" << G4endl;
+    G4cout << "  Total thickness: " << totalThick/cm << " cm" << G4endl;
+    G4cout << "  Transverse size: " << calorSize/cm << " cm" << G4endl;
+    G4cout << "====================================" << G4endl;
+    
+    return physWorld;
 }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-}  // namespace B1
+} // namespace B1
