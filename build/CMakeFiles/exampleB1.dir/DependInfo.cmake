3837aeb31369cae84de4c7c527eb2f7f26319180
vka228 <dvikultsev@gmail.com>
2026-06-18 10:55:05 +0300

    Initial commit

---
+++ build/CMakeFiles/exampleB1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/daniil/Geant4/geant4-v11.4.2-install/share/Geant4/examples/basic/calorimeter/exampleB1.cc" "CMakeFiles/exampleB1.dir/exampleB1.cc.o" "gcc" "CMakeFiles/exampleB1.dir/exampleB1.cc.o.d"
+  "/home/daniil/Geant4/geant4-v11.4.2-install/share/Geant4/examples/basic/calorimeter/src/ActionInitialization.cc" "CMakeFiles/exampleB1.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/exampleB1.dir/src/ActionInitialization.cc.o.d"
+  "/home/daniil/Geant4/geant4-v11.4.2-install/share/Geant4/examples/basic/calorimeter/src/DetectorConstruction.cc" "CMakeFiles/exampleB1.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/exampleB1.dir/src/DetectorConstruction.cc.o.d"
+  "/home/daniil/Geant4/geant4-v11.4.2-install/share/Geant4/examples/basic/calorimeter/src/EventAction.cc" "CMakeFiles/exampleB1.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/exampleB1.dir/src/EventAction.cc.o.d"
+  "/home/daniil/Geant4/geant4-v11.4.2-install/share/Geant4/examples/basic/calorimeter/src/PrimaryGeneratorAction.cc" "CMakeFiles/exampleB1.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/exampleB1.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/home/daniil/Geant4/geant4-v11.4.2-install/share/Geant4/examples/basic/calorimeter/src/RunAction.cc" "CMakeFiles/exampleB1.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/exampleB1.dir/src/RunAction.cc.o.d"
+  "/home/daniil/Geant4/geant4-v11.4.2-install/share/Geant4/examples/basic/calorimeter/src/SteppingAction.cc" "CMakeFiles/exampleB1.dir/src/SteppingAction.cc.o" "gcc" "CMakeFiles/exampleB1.dir/src/SteppingAction.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
